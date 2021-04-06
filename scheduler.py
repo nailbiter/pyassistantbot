43e680ee7155a9c688b986cb4abfabb1683cab96
Alex Leontiev <nailbiter@dtws-work.in>
2021-04-06 17:39:30 +0900

    stash

---
+++ scheduler.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""===============================================================================
+
+        FILE: scheduler.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2021-04-06T17:38:57.133197
+    REVISION: ---
+
+==============================================================================="""
+
+import click
+
+@click.command()
+def scheduler():
+    pass
+
+if __name__=="__main__":
+    scheduler()
