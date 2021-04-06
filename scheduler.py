7197f371b131bffb46f6f6a8d08fb9d498cd20d9
Alex Leontiev <nailbiter@dtws-work.in>
2021-04-06 19:26:20 +0900

    commit

--- scheduler.py
+++ scheduler.py
@@ -20,10 +20,38 @@ ORGANIZATION:
 ==============================================================================="""
 
 import click
+import sqlite3
+import time
+import pandas as pd
 
-@click.command()
-def scheduler():
+
+@click.group()
+@click.option("--database-file",type=click.Path(),default="scheduler.db")
+@click.option("--interval-min",type=int,default=5)
+@click.pass_context
+def scheduler(ctx,**kwargs):
+    ctx.ensure_object(dict)
+    ctx.obj["kwargs"] = {**kwargs}
+
+
+@scheduler.command()
+@click.pass_context
+def start(ctx,):
+    kwargs = ctx.obj["kwargs"]
+    while True:
+        time.sleep(60*kwargs["interval_min"])
+		conn = sqlite3.connect(kwargs["database_file"])
+		df=pd.read_sql_query("""SELECT * FROM tasks left""", conn)
+#		conn = sqlite3.connect(file_sqlite3)
+#		df.to_sql('cities',conn,if_exists='append',index=None)
+        conn.close()
+
+
+@scheduler.command()
+@click.pass_context
+def schedule(ctx,):
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     scheduler()
