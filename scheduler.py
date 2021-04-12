07024846475e5142616021ec7c26cea83ab4bff7
Alex Leontiev <nailbiter@dtws-work.in>
2021-04-12 17:14:38 +0900

    stash

--- scheduler.py
+++ scheduler.py
@@ -25,14 +25,28 @@ import time
 import pandas as pd
 from datetime import datetime
 import uuid
+import json
+import os
+import math
+from croniter import croniter
+import logging
+
+
+def _add_logger(f):
+    logger = logging.getLogger(f.__name__)
+
+    def _f(*args, **kwargs):
+        return f(*args, logger=logger, **kwargs)
+    _f.__name__ = f.__name__
+    return _f
 
 
 @click.group()
-@click.option("--database-file", type=click.Path(), default="scheduler.db")
-@click.option("--interval-min", type=int, default=1)
+@click.option("-d", "--database-file", type=click.Path(), default="scheduler.db")
+@click.option("-i", "--interval-min", type=int, default=1)
 @click.pass_context
 def scheduler(ctx, **kwargs):
-    assert kwargs["interval_min"] > 0
+    assert 60 > kwargs["interval_min"] > 0
     ctx.ensure_object(dict)
     ctx.obj["kwargs"] = {**kwargs}
 
@@ -68,19 +82,22 @@ def start(ctx,):
     _create_tables(kwargs["database_file"])
     while True:
         conn = sqlite3.connect(kwargs["database_file"])
-        now_ = datetime.now().strftime("%Y%m%d%H%M")
-        click.echo(f"now_: {now_}")
+        now_ = datetime.now()
+        click.echo(f"now_: {now_.strftime('%Y%m%d%H%M')}")
         df = pd.read_sql_query(
             f"""
             SELECT * 
             FROM tasks left join tasks_done using (task_id) left join tasks_cronlines using (cronline_id) 
-            WHERE is_done is null and due_date<="{now_}"
+            WHERE is_done is null and due_date<="{now_.strftime('%Y%m%d%H%M')}"
             """,
             conn
         )
         conn.close()
 
         for r in df.to_dict(orient="records"):
+            action = json.loads(r["action"])
+            if action["tag"] == "shell":
+                os.system(action["value"])
             click.echo(f"TODO: {r}")
 
         conn = sqlite3.connect(kwargs["database_file"])
@@ -88,11 +105,12 @@ def start(ctx,):
             'tasks_done', conn, if_exists='append', index=None)
         conn.close()
 
-        # FIXME: wait only if need to
-        time.sleep(60*kwargs["interval_min"])
+        seconds_to_sleep = interval_min*60 - (datetime.now()-now_).seconds
+        if seconds_to_sleep > 0:
+            time.sleep(seconds_to_sleep)
 
 
-def _schedule(database_file, due_date, action):
+def _schedule(database_file, due_date, action, cron_line=None):
     kwargs = {"due_date": due_date, "action": action}
     kwargs["due_date"] = kwargs["due_date"].strftime("%Y%m%d%H%M")
     conn = sqlite3.connect(database_file)
@@ -103,11 +121,33 @@ def _schedule(database_file, due_date, action):
 
 @scheduler.command()
 @click.pass_context
-@click.option("-a", "--action")
-@click.option("-d", "--due-date", type=click.DateTime())
-def schedule(ctx, **kwargs):
+@click.option("-t", "--tag", type=click.Choice(["shell"]), default="shell")
+@click.option("-v", "--value", required=True)
+@click.option("-d", "--due-date", type=click.DateTime(), required=True)
+@click.option("-c", "--cron-line")
+@click.option("--auto-fix/--no-auto-fix", default=False)
+@_add_logger
+def schedule(ctx, due_date, value, tag, auto_fix, cron_line, logger=None):
     kwargs_ = ctx.obj["kwargs"]
-    _schedule(kwargs["database_file"], **kwargs)
+    interval_min = kwargs_["interval_min"]
+    aligned_date = due_date.replace(minute=math.ceil(
+        due_date.minute/interval_min) * interval_min)
+    if due_date != aligned_date:
+        if auto_fix:
+            logger.info(
+                f"replace due_date {due_date.isoformat()} with {aligned_date.isoformat()}")
+            due_date = aligned_date
+        else:
+            assert False, f"replace due_date {due_date.isoformat()} with {aligned_date.isoformat()}"
+    if cron_line is not None:
+        c = croniter(cron_line)
+        d = c.get_next(datetime)
+        remainder = (c.get_next(datetime, start_time=d) -
+                     d).seconds % (60*interval_min)
+        assert remainder == 0, (cron_line, remainder)
+
+    _schedule(kwargs["database_file"], due_date=due_date,
+              action={"tag": tag, "value": value}, cron_line=cron_line)
 
 
 if __name__ == "__main__":
