952e3146a5c7cc7fa6e4f888889f24d3e52c9a32
Alex Leontiev <nailbiter@dtws-work.in>
2021-04-29 16:30:05 +0900

    stash

---
+++ start.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""===============================================================================
+
+        FILE: start.py
+
+       USAGE: ./start.py
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2021-04-29T16:18:19.680930
+    REVISION: ---
+
+==============================================================================="""
+
+import click
+
+@click.command()
+def start():
+    pass
+
+if __name__=="__main__":
+    start()
