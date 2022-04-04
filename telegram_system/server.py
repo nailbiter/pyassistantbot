73707753da3ed2af89ccd4cedb20ad0737eee5bd
Alex Leontiev <nailbiter@dtws-work.in>
2022-04-04 21:59:04 +0900

    stash

--- telegram_system/server.py
+++ telegram_system/server.py
@@ -37,7 +37,7 @@ def new_habit():
     message = json.loads(request.form["message"])
     chat_id = message["chat"]["id"]
     text = message["text"]
-    _, c1, c2, c3, c4, c5, media, msg = re.split(r"\s+", text, maxsplit=8)
+    _, c1, c2, c3, c4, c5, media, msg = re.split(r"\s+", text, 8)
     url = f"http://{os.environ['SCHEDULER']}/register_regular_call"
     _url = f"http://{os.environ['SELF_URL']}/send_message"
     payload = {
@@ -85,7 +85,7 @@ def new_timer():
     message = json.loads(request.form["message"])
     chat_id = message["chat"]["id"]
     text = message["text"]
-    _, time, media, msg = re.split(r"\s+", text, maxsplit=4)
+    _, time, media, msg = re.split(r"\s+", text, 4)
     url = f"http://{os.environ['SCHEDULER']}/register_single_call"
     _url = f"http://{os.environ['SELF_URL']}/send_message"
     payload = {"text": msg, "chat_id": chat_id}
